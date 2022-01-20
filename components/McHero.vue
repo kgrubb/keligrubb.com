32e7e104dc62abc4e54a224263da279ebc171302
Keli Grubb <keligrubb324@gmail.com>
2022-01-20 17:29:23 -0500

    update dependencies and fix linting errors

--- components/McHero.vue
+++ components/McHero.vue
@@ -2,10 +2,14 @@
   <section class="hero is-primary is-bold is-fullheight">
     <div class="hero-body has-bg-img">
       <div class="container is-fluid has-text-centered">
-        <h1 class="title is-1">Want to play some minecraft with me?</h1>
+        <h1 class="title is-1">
+          Want to play some minecraft with me?
+        </h1>
         <h1
           class="title is-3 has-text-weight-light has-text-centered"
-        >Download my modpack & follow its instructions to get started!</h1>
+        >
+          Download my modpack & follow its instructions to get started!
+        </h1>
         <a
           class="button is-primary is-inverted is-outlined"
           :href="getLatestRelease"

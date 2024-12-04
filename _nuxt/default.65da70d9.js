6f2bc4c9e818df9ce1de35789892080339315d49
kgrubb <kgrubb@users.noreply.github.com>
2024-12-04 20:39:49 +0000

    deploy: 4ab5fe1fe24bbcbc1f3f35b2d9a6165b55e1e247

---
+++ _nuxt/default.65da70d9.js
@@ -0,0 +1,1 @@
+import{_ as t,N as _}from"./entry.0bf9b9af.js";const n={};function r(e,s){return _(e.$slots,"default")}const o=t(n,[["render",r]]);export{o as default};
--- _nuxt/designed_for_knave.e1fd6ce9.js
+++
@@ -1,1 +0,0 @@
-import"./entry.ecc175f8.js";const r=""+new URL("designed_for_knave.f1a63ef3.png",import.meta.url).href;export{r as _};
